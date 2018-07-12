eda8b706554080f080883db62d3d7c9f531f44a2
AarthiJanaRanjini <41136798+AarthiJanaRanjini@users.noreply.github.com>
2018-07-12 10:45:42 +0530

    Add files via upload

---
+++ posneg.java
@@ -0,0 +1,23 @@
+	import java.util.Scanner;
+	public class posneg{
+
+	public static void main(String[] args) {
+
+
+int a;
+	a=Integer.parseInt(args[0]);
+if(a==0){
+	System.out.println("zero");
+
+}
+else if(a>0){
+System.out.println("positive");
+
+}
+
+else
+{
+System.out.println("negative");
+}
+}
+	}
