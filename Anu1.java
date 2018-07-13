150f3f209c7e477921479d71045b9f312134ef9c
AarthiJanaRanjini <41136798+AarthiJanaRanjini@users.noreply.github.com>
2018-07-13 17:50:24 +0530

    Create Anu1.java

---
+++ Anu1.java
@@ -0,0 +1,14 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		char ch='A';
+		if(ch>='a'&&ch<='z'||ch>='A'&&ch<='Z'){
+			System.out.println("Alphabet");
+			}
+			else{
+				System.out.println("Not");
+			}
+			}
+}
