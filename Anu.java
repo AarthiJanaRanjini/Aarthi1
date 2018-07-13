99bf545993b1dfbf69edf3cc927b8d80b594bd2c
AarthiJanaRanjini <41136798+AarthiJanaRanjini@users.noreply.github.com>
2018-07-13 17:36:18 +0530

    Create Anu.java

---
+++ Anu.java
@@ -0,0 +1,14 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+	char ch='*';
+	if(ch=='a' ||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+		System.out.println("Vowel");
+	}
+		else{
+			System.out.println("Invalid");
+		}
+		}
+                         }
