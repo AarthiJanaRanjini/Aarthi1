cb1b41736db8b84a305438d1bd0ac3769be68731
AarthiJanaRanjini <41136798+AarthiJanaRanjini@users.noreply.github.com>
2018-07-20 14:14:40 +0530

    Create prime number.java

---
+++ prime number.java
@@ -0,0 +1,22 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int a=15;int count=0;
+		for(int i=1;i<=a;i++){
+		if(a%i==0){
+		            count++;
+			}
+		}
+			if(count==2){
+				System.out.println("prime number");
+				}
+		
+		else {
+			System.out.println("not prime");
+		}
+		
+		
+	}
+}
