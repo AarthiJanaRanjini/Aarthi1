8c396393a5415aedc8da41218985ac7d745fb91e
AarthiJanaRanjini <41136798+AarthiJanaRanjini@users.noreply.github.com>
2018-07-20 10:59:10 +0530

    Create Hello n.java

---
+++ Hello n.java
@@ -0,0 +1,13 @@
+lass Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+ 
+		String a="Hello";
+		int n=5;
+		for(int i=0;i<n;i++){
+			System.out.println("Hello");
+	}
+  }
+}
