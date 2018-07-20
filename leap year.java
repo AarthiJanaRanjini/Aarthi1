24d1e9f76d37d28b0e9b750158b72b4ce4beb053
AarthiJanaRanjini <41136798+AarthiJanaRanjini@users.noreply.github.com>
2018-07-20 10:54:49 +0530

    Create leap year.java

---
+++ leap year.java
@@ -0,0 +1,18 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+
+int year =2017;
+if(year%4==0){
+	System.out.println(year + "is leap year");
+}
+else{
+	System.out.println(year+ "is not a leap year");
+}
+		
+		
+		
+	}
+}
