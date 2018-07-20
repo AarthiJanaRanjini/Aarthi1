2106dc486866052abe0f84e63b3cf0c992948570
AarthiJanaRanjini <41136798+AarthiJanaRanjini@users.noreply.github.com>
2018-07-20 15:29:28 +0530

    Create divide five.java

---
+++ divide five.java
@@ -0,0 +1,13 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+	int a=25;
+	for(int i=1;i<=a;i++){
+		if(i%5==0){
+			System.out.println(i);
+		}
+	}	
+	}
+}
