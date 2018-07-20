fb383e05067a6b43621e1c08a316d546b1b371f2
AarthiJanaRanjini <41136798+AarthiJanaRanjini@users.noreply.github.com>
2018-07-20 17:31:25 +0530

    Create larger num.java

---
+++ larger num.java
@@ -0,0 +1,21 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+	int arr[]=new int[10];
+	arr[0]=56;
+	arr[1]=89;
+	arr[2]=12;
+	if(arr[0]>arr[1]&&arr[0]>arr[2]){
+		System.out.println(arr[0]);
+	}
+	else if(arr[1]>arr[0]&&arr[1]>arr[2]){
+		System.out.println(arr[1]);
+	}
+		else if(arr[2]>arr[0]&&arr[2]>arr[1]){
+			System.out.println(arr[2]);
+		}
+	}
+	
+}
