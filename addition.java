10dbf72ae6bffc11be7345b626cf3cf107b33299
AarthiJanaRanjini <41136798+AarthiJanaRanjini@users.noreply.github.com>
2018-07-20 11:13:28 +0530

    Create addition.java

---
+++ addition.java
@@ -0,0 +1,14 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		
+		int a=3;int sum=0;
+		for(int i=0;i<=a;i++){
+			sum+=i;
+		}
+			System.out.println(sum);
+	
+	}
+}
