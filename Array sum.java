5180ff06cebe3c09efa23181ca3a9282f8c36a3d
AarthiJanaRanjini <41136798+AarthiJanaRanjini@users.noreply.github.com>
2018-07-20 11:35:36 +0530

    Create Array sum.java

---
+++ Array sum.java
@@ -0,0 +1,13 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n=2;int sum=0;
+		int arr[]={3,8,2,9,1};
+		for(int i=0;i<n;i++){
+	            sum=sum+arr[i];
+		}
+		System.out.println(sum);
+	}
+}
