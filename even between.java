3c550621a3b86245c239e7b12bd8599bcda6881c
AarthiJanaRanjini <41136798+AarthiJanaRanjini@users.noreply.github.com>
2018-07-20 14:27:39 +0530

    Create even between.java

---
+++ even between.java
@@ -0,0 +1,14 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+       int a=5;int b=10;
+       for(int i=a+1;i<b;i++){
+       	if(i%2==0){
+       		System.out.println(i);
+       	}
+        }
+ 
+	}
+}
