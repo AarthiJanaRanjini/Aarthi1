7d782639241c786a225eed102ba6135c4f0ae176
AarthiJanaRanjini <41136798+AarthiJanaRanjini@users.noreply.github.com>
2018-07-20 12:05:02 +0530

    Create power.java

---
+++ power.java
@@ -0,0 +1,11 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+	int a=2;
+	int b=3;
+	double a1=(int)Math.pow(a,b);
+	System.out.println(a1);
+			}
+}
