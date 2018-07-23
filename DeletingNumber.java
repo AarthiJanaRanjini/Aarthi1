4bfd33d9fcf2e53e3a533945df833ee2f8a5baa4
AarthiJanaRanjini <41136798+AarthiJanaRanjini@users.noreply.github.com>
2018-07-23 18:18:41 +0530

    Create DeletingNumber.java

---
+++ DeletingNumber.java
@@ -0,0 +1,14 @@
+public class DeletingNumber {
+public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+long a=in.nextLong();
+	int b=in.nextInt();
+	int i,j,k=0;int s,c=0;
+String a1=Long.toString(a);
+char ch[]=a1.toCharArray();
+for(i=b;i<ch.length;i++){
+	System.out.print(ch[i]);
+}
+}
+}
+
