df734f3248f9a978eb6a264ccbf8f2f12af18f64
AarthiJanaRanjini <41136798+AarthiJanaRanjini@users.noreply.github.com>
2018-07-24 18:22:24 +0530

    Create Guvi20.java

---
+++ Guvi20.java
@@ -0,0 +1,19 @@
+public class Guvi20 {
+
+	public static void main(String[] args) {
+Scanner in=new Scanner(System.in);
+int a=in.nextInt();
+int b=in.nextInt();
+int c=in.nextInt();
+int d=b+c;
+if(a%2==0){
+	if(a%d==0){
+		System.out.println("yes");
+	}
+}
+else{
+	System.out.println("no");
+}
+		}
+
+}
