cc2cfde60402104572425bb6ee6b364c5b197308
AarthiJanaRanjini <41136798+AarthiJanaRanjini@users.noreply.github.com>
2018-07-27 17:11:23 +0530

    Create 2No-1.java

---
+++ 2No-1.java
@@ -0,0 +1,12 @@
+public class GuviNum{
+
+	public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	int a=in.nextInt();
+	int c=1;
+for(int i=1;i<a;i++){
+	c*=i;
+}
+		System.out.println(c);
+		}
+    }
