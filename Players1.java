1f24f5d922ca6562040bd6b33da70d8cb7ca7af2
AarthiJanaRanjini <41136798+AarthiJanaRanjini@users.noreply.github.com>
2018-07-27 10:46:22 +0530

    Rename Players.java to Players1.java

---
+++ Players1.java
@@ -0,0 +1,19 @@
+public class GuviPlayers {
+
+	public static void main(String[] args) {
+Scanner in=new Scanner(System.in);
+int a=in.nextInt();
+int c=1,d=1;
+if(a%2!=0){
+while(a>0){
+	c*=a;
+	a--;
+	}
+System.out.println(1);
+}
+else {
+	System.out.println(0);
+}
+		}
+
+}
